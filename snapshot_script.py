ed59df744ebb2f8aac21c48b61b11dee30ebd0e6
David Grisales <davidgrisales@Davids-MacBook-Air-3.local>
2026-03-16 18:17:15 -0500

    feature: snapshot upload to S3

--- snapshot_script.py
+++ snapshot_script.py
@@ -13,6 +13,7 @@ dbname = os.getenv("DB_NAME")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 
+file_path_global = "snapshots/"
 
 def create_snapshot():
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -24,14 +25,29 @@ def create_snapshot():
         '-d', dbname,
         "-U", user,
         "-F", "c",
-        '-f', filename
+        '-f', f"{file_path_global}{filename}"
     ]
     env_copy = os.environ.copy()
     env_copy["PGPASSWORD"] = password
     try:
         subprocess.run(cmd, check=True, env=env_copy)
         print(f"Snapshot created successfully: {filename}")
+        return filename
     except subprocess.CalledProcessError as e:
         print(f"Error creating snapshot: {e}")
+        return None
+
+def upload_to_s3(filename):
+    import boto3
+    s3 = boto3.client('s3')
+    bucket_name = os.getenv("BUCKET_NAME")
+    file_path = f"{file_path_global}{filename}"
+    try:
+        s3.upload_file(file_path, bucket_name, filename)
+        logging.info(f"File {filename} uploaded to S3 bucket {bucket_name} successfully.")
+    except Exception as e:
+        logging.error(f"Error uploading file to S3: {e}")
 if __name__ == "__main__":
-    create_snapshot()
+    filename = create_snapshot()
+    if filename:
+        upload_to_s3(filename)
